3652df77282edddd28a5b4299e0084e091d4fe21
kali <txmm@pm.me>
2022-12-26 01:05:03 -0500

    fix parameter indexing problem

--- src/Solvers/simu/simu_entry.py
+++ src/Solvers/simu/simu_entry.py
@@ -9,7 +9,7 @@ with open("simu_inputs.txt", "r") as f:
 task_id = int(os.environ["SLURM_ARRAY_TASK_ID"])
 print(f"Hello! This is Simulated Annealing - Task {task_id}")
 
-seed, mode = lines[task_id].split(',')
+seed, mode = lines[task_id-1].split(',')
 
 timeout = 1200
 simu_main(seed, mode, timeout)
