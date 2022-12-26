45dad20b6b99adc5ce2b6681458355ac4953614f
Raresh Nistor <raresh@nistor.email>
2022-12-26 00:36:56 -0500

    pip shenanigans

--- jobs/batch.sh
+++ jobs/batch.sh
@@ -4,6 +4,8 @@
 module load python/3.8.1
 python3 -m venv ../venv
 source ../venv/bin/activate
+
+python -m pip upgrade pip
 pip3 install -r ../requirements.txt
 
 # kick of each thing we're testing in parallel
