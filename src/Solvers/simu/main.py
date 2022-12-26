e8496aaa6e6bce6697d097bc41499e49f04d6cf8
kali <txmm@pm.me>
2022-12-26 04:30:09 -0500

    refactor sim_anneal to make grid computers go brrrrr

--- src/Solvers/simu/main.py
+++ src/Solvers/simu/main.py
@@ -1,6 +1,8 @@
 import csv
 import os
 
+import numpy
+
 from jobshop import *
 
 
@@ -10,6 +12,11 @@ def simu_main(seed=None, temp=None, cooldowwn=None, timeout=None, instance=None)
 
     if seed is None:
         seed = int(input("seed: ") or 0)
+
+    # Set the seed
+    random.seed(seed)
+    numpy.random.seed(seed)
+
     if temp is None:
         temp = int(input("temp: ") or 200)
     if cooldowwn is None:
@@ -19,42 +26,45 @@ def simu_main(seed=None, temp=None, cooldowwn=None, timeout=None, instance=None)
 
     scores = {}
 
-    for file in files:
-        if file == instance:
-            jobs = Jobs('instances/' + file)
+    # for file in files:
 
-            m = len(jobs[0])
-            j = len(jobs)
-            print("Chosen file:", file)
-            print("Number of machines:", m)
-            print("Number of jobs:", j)
-            printJobs(jobs)
+    jobs = Jobs('instances/' + instance)
 
-            (cost, solution), timedOut = simulatedAnnealingSearch(jobs,
-                                                                  maxTime=timeout,
-                                                                  T=int(temp),
-                                                                  termination=int(10),
-                                                                  halting=int(10),
-                                                                  mode='random',
-                                                                  decrease=float(cooldowwn))
+    m = len(jobs[0])
+    j = len(jobs)
+    print("Chosen file:", instance)
+    print("Number of machines:", m)
+    print("Number of jobs:", j)
+    printJobs(jobs)
 
-            printSchedule(jobs, solution)
-            print("TEST:" + str(cost), "Timed Out?" + str(timedOut))
-            # @Todo Solutions are not being saved
-            scores[file] = cost
-        print(scores)
-        csv_columns = files
+    (cost, solution), timedOut = simulatedAnnealingSearch(jobs,
+                                                          maxTime=timeout,
+                                                          T=int(temp),
+                                                          termination=int(10),
+                                                          halting=int(10),
+                                                          mode='random',
+                                                          decrease=float(cooldowwn))
 
-        csv_file = f"{os.environ['OUTPUT_DIR']}/scores-{os.environ['SLURM_ARRAY_TASK_ID']}.csv"
-        try:
-            with open(csv_file, 'w') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-                writer.writeheader()
-                writer.writerow(scores)
-        except IOError:
-            print("I/O error")
+    printSchedule(jobs, solution)
+    print("TEST:" + str(cost), "Timed Out?" + str(timedOut))
+    # @Todo Solutions are not being saved (Output is saved)
 
+    csv_columns = instance, cost, seed, temp, cooldowwn, timeout
+
+    csv_file = f"{os.environ['OUTPUT_DIR']}/scores-{os.environ['SLURM_ARRAY_TASK_ID']}.csv"
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in csv_columns:
+                writer.writerow(data)
+
+
+
+    except IOError:
+        print("I/O error")
 
 
 if __name__ == '__main__':
     simu_main()
+    print("Done")
