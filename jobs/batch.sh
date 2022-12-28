18b938179133515a2d90b4e44d559266111a4985
kali <txmm@pm.me>
2022-12-28 07:46:41 -0500

    testing sim anneal

--- jobs/batch.sh
+++ jobs/batch.sh
@@ -11,14 +11,14 @@ pip3 install -r ../requirements.txt
 # kick of each thing we're testing in parallel
 #sbatch constraint.sh
 #sbatch genetic.sh
-#sbatch sim_anneal.sh
-
-TABU_RDIR="output/tabu_search"
-TABU_DATE="$(date +%Y-%m-%d_%H-%M-%S)"
-export TABU_RUNDIR="$TABU_RDIR/$TABU_DATE"
-mkdir -p "$TABU_RUNDIR"/{img,json}
-
-
-
-sbatch tabu_search.sh
+sbatch sim_anneal.sh
+#
+#TABU_RDIR="output/tabu_search"
+#TABU_DATE="$(date +%Y-%m-%d_%H-%M-%S)"
+#export TABU_RUNDIR="$TABU_RDIR/$TABU_DATE"
+#mkdir -p "$TABU_RUNDIR"/{img,json}
+#
+#
+#
+#sbatch tabu_search.sh
 
