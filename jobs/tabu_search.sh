c3c381008b0191b89bf6d898ff6e8e96e4f833bd
kali <txmm@pm.me>
2022-12-28 06:59:25 -0500

    fix relative path

--- jobs/tabu_search.sh
+++ jobs/tabu_search.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -a 1-1001
+#SBATCH -a 1-20
 #SBATCH -o stdio/tabu_search-stdout-%A-%a.txt
 #SBATCH -e stdio/tabu_search-stderr-%A-%a.txt
 #SBATCH --cpus-per-task=1
@@ -16,6 +16,6 @@ cd ../src/Solvers/tabu || exit 1
 
 # Env var for the run directory is set in batch.sh as "TABU_RUNDIR"
 
-OUTPUT_DIR=$TABU_RUNDIR python3 tabu_entry.py
+OUTPUT_DIR="../../../jobs/$TABU_RUNDIR" python3 tabu_entry.py
 # To get through all instances you have to manually batch the slurm ids, ie 1-1001, 1002-2002, 2003-3003, etc.
 # Match the number of ids to the number of inputs in the file for that script
