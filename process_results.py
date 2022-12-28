5541efcedb1349e2833fea0aacd7694c3780d842
kali <txmm@pm.me>
2022-12-28 08:25:30 -0500

    Plots wont work on grid have to render locally.

--- process_results.py
+++ process_results.py
@@ -1,7 +1,9 @@
-import os
-import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
+import os
+import json
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt
 
 
 def pretty_plots(instance, dir, key):
@@ -24,11 +26,8 @@ def pretty_plots(instance, dir, key):
 
     plt.figure()
 
-
-
     # Boxplot for overall performance with all seeds combined
     box_plotter(df, dir, instance, "Overall performance", X="param", Y="cost")
-
 
     # Create a boxplot for each
     box_plotter(df, dir, instance, "Cost variance across seeds for " + instance, X="seed", Y="cost")
@@ -77,13 +76,63 @@ def unify_csvs(dir, key):
     os.system(f"cp {dir}/results_sorted.csv {result_dir}/results_sorted.csv")
 
 
+def render_gantt_json(file, outdir):
+    with open(file, "r") as f:
+        gantt_data = json.load(f)
+
+    list_of_dicts = []
+    for job in gantt_data["packages"]:
+        d = {"Machine": job["label"],
+             "Start": job["start"],
+             "End": job["end"],
+             "Color": job["color"],
+             "Job": job["legend"]}
+
+        list_of_dicts.append(d)
+
+    df = pd.DataFrame(list_of_dicts)
+    df['Delta'] = df['End'] - df['Start']
+
+    fig = px.timeline(df, x_start="Start", x_end="End", y="Machine", color="Color", labels="Job",
+                      title=gantt_data["title"])
+    fig.update_yaxes(autorange="reversed")
+    fig.layout.xaxis.type = 'linear'
+
+    for d in fig.data:
+        filt = df['Color'] == d.name
+        d.name = str(df[filt]['Job'].values[0])
+        d.x = df[filt]['Delta'].tolist()
+
+    fig.update_xaxes(type='linear')
+    fig.update_yaxes(autorange="reversed")  # otherwise tasks are listed from the bottom up
+    # Read the name of the file
+    name = file.split("/")[-1].split(".")[0]
+    slurm = name.split("_")[0]
+
+    fig.write_image(outdir + "/img/gantt-" + slurm + ".png", format="png")
+
+
 if __name__ == '__main__':
-    dir = "/home/kali/PycharmProjects/Capstone/jobs/output/sim_anneal"
-    key = "instance,cost,seed,temp,cooldown,timeout"
-    unify_csvs(dir=dir, key=key)
 
-    datasets = [f for f in os.listdir(dir) if f.startswith('results') is False and f.endswith('.csv')]
-    for dataset in datasets:
-        pretty_plots(instance=dataset, dir=dir, key=key)
+    outdir = "/home/kali/PycharmProjects/Capstone/jobs/output/"
+    sim_dir = outdir + "sim_anneal/"
+    tabu_dir = outdir + "tabu_search/"
 
-    # pretty_plots(instance="results_sorted.csv", dir=dir, key=key)
+
+    ### SIMULATED ANNEALING ###
+    # key = "instance,cost,seed,temp,cooldown,timeout"
+    # unify_csvs(dir=sim_dir, key=key)
+    #
+    # datasets = [f for f in os.listdir(sim_dir) if f.startswith('results') is False and f.endswith('.csv')]
+    # for dataset in datasets:
+    #     pretty_plots(instance=dataset, dir=sim_dir, key=key)
+    #
+    # # pretty_plots(instance="results_sorted.csv", dir=dir, key=key)
+
+    ### TABU SEARCH GANTT ###
+    # for file in os.listdir(tabu_dir + "{DATE}/json/"):
+    candidates = tabu_dir + "json/"
+
+    files = [f for f in os.listdir(candidates) if f.endswith('.json')]
+    for file in files:
+        render_gantt_json(file=candidates + file, outdir=tabu_dir)
