088aba21b53a796070633859a1b97ebbe5115611
kali <txmm@pm.me>
2022-12-29 13:54:57 -0500

    This will have broken other stuff but I have nice gif animated gantt charts

--- process_helpers/process_results.py
+++ process_helpers/process_results.py
@@ -1,6 +1,110 @@
+import glob
 import os
+from PIL import Image
+import pandas as pd
 
-from process_helpers.utilities.plotting import alg_gantts, alg_merge_boxes
+from process_helpers.utilities.plotting import alg_gantts, alg_merge_boxes, render_gantt_json
+
+
+def gantt_gif_by_dataset(dataset, key, target_dir):
+    """
+    Identify unique schedules and create a gif for the given dataset
+    """
+
+    print("\nCreating Gantt GIF for: " + dataset)
+    unique_schedule_ids = None
+    csv_path = f"{target_dir}/results/{dataset}.csv"
+    # Columns
+    cols = key.split(",") + ["schedule_id"]
+    # Load the file
+    ids = unique_slurm_ids(cols, csv_path)
+
+    # If there are no unique schedules then we can't make a gif
+    if len(ids) == 0:
+        print(f"\tNo unique schedules found for {dataset}")
+        return
+
+    #
+    # Create folders
+    # {target_dir}/gif/{dataset}/
+    # If {target_dir}/gif/{dataset}/ does not exist, create it
+    img_path = f"{target_dir}/img/gif/{dataset}/"
+
+
+    if not os.path.exists(img_path):
+        os.makedirs(img_path)
+
+    for slurm in ids:
+        json_path = f"{target_dir}json/{slurm}_gantt.json"
+        render_gantt_json(file=json_path, outdir=img_path, img_subdir=False)
+
+    # Create the gif
+
+    frames = []
+    path = img_path + "*.png"
+    imgs = glob.glob(path)
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+
+    # Save into a GIF file that loops forever
+    frames[0].save(target_dir + dataset, format='GIF',
+                   append_images=frames[1:],
+                   save_all=True,
+                   duration=300, loop=0)
+
+
+
+def unique_slurm_ids(cols, csv_path):
+    df = pd.read_csv(csv_path, names=cols)
+    # Total Rows
+    total_rows = len(df.index)
+    # Count how often each cost appears
+    makespans = df["cost"].value_counts()
+    # print(makespans)
+    # Keep the makespans that appear only once
+    single_appearance = makespans[makespans == 1]
+    # print(f"Found {len(single_appearance)} costs that only appear once")
+    # print(f"\t\t{total_rows - len(single_appearance)} appear more than once")
+    # Get the schedule ids where the cost is in the unique_makespans
+    unique_schedule_ids = df[df["cost"].isin(single_appearance.index)]["schedule_id"]
+    # print(unique_schedule_ids)
+    # @TODO, look at the other schedules to find remaining unique schedules
+    # Could json dump the "Packages part of the json
+    return unique_schedule_ids
+
+
+def animated_gantt(target_dir, key):
+    """
+    Make a gif to show each unique gantt chart
+    """
+
+    # To create the animation we need to identify unique schedules
+    # if a schedule is the same then it will have the same makespan. but not all makespans are the same schedule.
+    datasets = ['ft10', 'abz7', 'ft20', 'abz9', 'la04', 'la03', 'abz6', 'la02', 'abz5', 'la01']
+    for dataset in datasets:
+        gantt_gif_by_dataset(dataset, key, target_dir)
+
+    # Merge
+    # Read all pngs from target dir and combine them into one gif
+    # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+
+    # Create the frames
+    frames = []
+    path = target_dir + "img/gif/*/*.png"
+    imgs = glob.glob(path)
+    for i in imgs:
+        new_frame = Image.open(i)
+        frames.append(new_frame)
+
+    # Save into a GIF file that loops forever
+    frames[0].save(target_dir + "ALL.gif", format='GIF',
+                     append_images=frames[1:],
+                        save_all=True,
+                        duration=300, loop=0)
+
+
+
 
 if __name__ == '__main__':
     outdir = "/home/kali/PycharmProjects/Capstone/jobs/output/"
@@ -17,6 +121,11 @@ if __name__ == '__main__':
     # key = "instance,cost,seed,tabu_length,max_steps,longest_hold,timeout"
     # alg_merge_boxes("TA", tabu_dir + "results/", key)
 
+    # Genetic
+    key = "instance,cost,seed,pop_size,ngen,mut_rate,cross_rate,timeout"
+    # alg_merge_boxes("GA", genetic_dir + "results/", key)
+    animated_gantt(genetic_dir, key)
+
     # # # Gantt Charts
     # alg_gantts(tabu_dir + "json/", tabu_dir)
     #
@@ -24,6 +133,8 @@ if __name__ == '__main__':
 
     # alg_gantts(constraint_dir + "json/", constraint_dir)
 
-    alg_gantts(sim_dir + "json/", sim_dir)
+    # alg_gantts(sim_dir + "json/", sim_dir)
+
+    # Animated Different Gantt Charts
 
     print("Done!")
--- process_helpers/utilities/plotting.py
+++ process_helpers/utilities/plotting.py
@@ -9,14 +9,15 @@ import matplotlib.pyplot as plt
 def alg_merge_boxes(alg, filepath, key):
     print("Merging CSVs for Boxplots: " + alg)
 
+    # UNCOMMENT IF NEED
     unify_csvs(filepath, key=key)
 
-    datasets = [f for f in os.listdir(filepath) if f.startswith('results') is False and f.endswith('.csv')]
-    for dataset in datasets:
-        pretty_plots(instance=dataset, target_dir=filepath, key=key, alg=alg)
-
-    pretty_plots(instance="results_sorted.csv", target_dir=filepath, key=key, alg=alg, verbose=True)
-
+    # datasets = [f for f in os.listdir(filepath) if f.startswith('results') is False and f.endswith('.csv')]
+    # for dataset in datasets:
+    #     pretty_plots(instance=dataset, target_dir=filepath, key=key, alg=alg)
+    #
+    # pretty_plots(instance="results_sorted.csv", target_dir=filepath, key=key, alg=alg, verbose=True)
+    #
 
 
 def pretty_plots(instance, target_dir, key, alg, verbose=False):
@@ -43,6 +44,14 @@ def pretty_plots(instance, target_dir, key, alg, verbose=False):
         df['param'] = df['tabu_length'].astype(str) + "-" + df['max_steps'].astype(str) + "-" + df[
             'longest_hold'].astype(
             str)
+    elif alg == "GA":
+        # pop_size,ngen,mut_rate,cross_rate
+        df['param'] = df['pop_size'].astype(str) + "-" + df['mut_rate'].astype(
+            str) + "-" + df['cross_rate'].astype(str)
+    # # Count unique parameter combinations
+    # params = df['param'].unique()
+    # print(f"Found {len(params)} parameter combinations")
+    # input("Press Enter to continue...")
 
     # Boxplot for overall performance with all seeds combined
     box_plotter(df, target_dir, instance, alg + "Cost variance across seeds for " + instance, X="seed", Y="cost")
@@ -62,9 +71,17 @@ def alg_gantts(candidates, filepath):
         render_gantt_json(file=candidates + file, outdir=filepath)
 
 
+def root_dir():
+    """
+    Dir above jobs
+    """
+    return os.getcwd()[0:os.getcwd().index("Capstone") + len("Capstone")]
+
+
 def box_plotter(df, target_dir, instance, name, X="param", Y="cost",):
     #@TODO Make sure scale is the same across all plots (y axis) so that they can be compared
-    result_dir = os.getcwd() + "/jobs/results/" + target_dir.split("/")[-3]
+
+    result_dir = root_dir() + "/jobs/results/" + target_dir.split("/")[-3]
     sns.boxplot(x=X, y=Y, data=df)
     # Rotate x labels
     plt.xticks(rotation=90)
@@ -76,6 +93,7 @@ def box_plotter(df, target_dir, instance, name, X="param", Y="cost",):
 def unify_csvs(filepath, key):
     print("Unifying csvs")
     print("Trying for dir " + filepath.split("/")[-3])
+    print("Full path:" + filepath)
     # Delete the files we create if the script has been run before
     # If it is prefixed with results keep it, if it is not, delete it
     for file in os.listdir(filepath):
@@ -92,7 +110,11 @@ def unify_csvs(filepath, key):
         for file in files:
             with open(f"{filepath}/{file}", "r") as file:
                 for line in file:
+                    slurm = file.name.split("/results-")[-1].split(".csv")[0]
+                    line = line.strip() + "," + slurm + "\n"
                     results_file.write(line)
+
+
     # Sort rows by instance
     os.system(f"sort -t, -k1,1 {filepath}/results.csv > {filepath}/results_sorted.csv")
 
@@ -101,13 +123,13 @@ def unify_csvs(filepath, key):
 
     # Create a separate file for each instance, overwriting if it exists
     os.system(f"awk -F, '{{print > \"{filepath}/\"$1\".csv\"}}' {filepath}/results_sorted.csv")
-    result_dir = os.getcwd() + "/jobs/results/" + filepath.split("/")[-3]
+    result_dir = root_dir() + "/jobs/results/" + filepath.split("/")[-3]
 
     # Copy the sorted file to the results directory
     os.system(f"cp {filepath}/results_sorted.csv {result_dir}/results_sorted.csv")
 
 
-def render_gantt_json(file, outdir):
+def render_gantt_json(file, outdir, img_subdir=True):
     with open(file, "r") as f:
         gantt_data = json.load(f)
 
@@ -140,4 +162,10 @@ def render_gantt_json(file, outdir):
     name = file.split("/")[-1].split(".")[0]
     slurm = name.split("_")[0]
 
-    fig.write_image(outdir + "/img/gantt-" + slurm + ".png", format="png")
+
+    if img_subdir:
+        dest = outdir + "/img/gantt-"
+    else:
+        dest = outdir + "/gantt-"
+
+    fig.write_image(dest + slurm + ".png", format="png")
